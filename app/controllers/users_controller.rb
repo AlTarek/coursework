3aaf52ddedded4be657745647c21efdc2dcdc956
Tarek Alhariri <thariri@qatar.cmu.edu>
2013-03-23 14:57:34 +0300

    Added User model and fixed DB and sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  def show
+  	@user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save 
+  		flash[:notice] = "New User Saved"
+  		redirect_to @user
+  	else
+  		render :action => "new"
+  	end
   end
 end
